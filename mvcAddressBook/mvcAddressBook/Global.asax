25f063dfbe61983de94108e4639fd0659349aaf3
Jessica Aquino <j-aquino@outlook.com>
2019-09-20 22:19:29 -0400

    Creación de solución MVC

---
+++ mvcAddressBook/mvcAddressBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcAddressBook.MvcApplication" Language="C#" %>
---
+++ mvcAddressBook/mvcAddressBook/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace mvcAddressBook.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcAddressBook.Models.Book> Books { get; set; }
+    }
+}
