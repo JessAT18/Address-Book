fcefeda56bdb922fc6efe92e42e38249f1ac3b9a
Jessica Aquino <j-aquino@outlook.com>
2019-09-23 01:28:06 -0400

    Creación correcta de WebAPI

---
+++ mvcAddressBook/apiAddressBook/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace apiAddressBook.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        twitter,
+        instagram,
+        dblink
+    }
+    public class Book
+    {
+        [Key]
+        public int bookID { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+    }
+}
---
+++ mvcAddressBook/apiAddressBook/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace apiAddressBook.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiAddressBook.Models.Book> Books { get; set; }
+    }
+}
