e9f870fce399fa13e0d982e8cb32feb05b41d6db
Jessica Aquino <j-aquino@outlook.com>
2019-10-05 10:16:52 -0400

    Modificación de archivos appAddress

---
+++ mvcAddressBook/appAddress/appAddress/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appAddress.Models
+{
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        twitter,
+        instagram,
+        dblink
+    }
+    public class Book
+    {
+        public int bookID { get; set; }
+        public string name { get; set; }
+        public TypeContact type { get; set; }
+        public string contact { get; set; }
+    }
+}
